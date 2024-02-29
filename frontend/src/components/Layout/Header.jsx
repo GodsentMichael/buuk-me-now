cb288ca3278200a708ba5ae218447c4a6be5bb4b
Godsent Michael <gentlemotivator@gmail.com>
2024-02-29 17:50:31 +0100

    updates done.

--- frontend/src/components/Layout/Header.jsx
+++ frontend/src/components/Layout/Header.jsx
@@ -6,23 +6,31 @@ import Navbar from "../Layout/Navbar";
 
 const Header = ({ active }) => {
   const [dropdownActive, setDropdownActive] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
 
   return (
     <div className={`bg-[#FFFFFF] px-6 pt-4 pb-4 ${styles.section}`}>
-      <div className="hidden 800px:h-[50px] 800px:my-[20px] 800px:flex items-center justify-between">
+      <div className="flex items-center justify-between">
         <div>
           <Link to="/">
             <img
               src="/src/assets/book-me-logo.png"
               className="bg-transparent h-12"
               alt="logo"
+              style={{ width: "130px", height: "auto"}}
             />
           </Link>
         </div>
+        <div className="hidden md:block">
+        <Navbar active={active}/>
+        </div>
+         
         {/*The navitems */}
-        <Navbar active={3} />
-
-        <div className="relative">
+        <div className="hidden md:flex">
           <button className="button flex  items-center border p-2 px-4 rounded-md w-auto">
             <div className="flex flex-col">
               <span className="font-bold">
@@ -37,6 +45,7 @@ const Header = ({ active }) => {
             />
           </button>
           {dropdownActive && (
+            <>
             <div className="absolute top-full left-0 mt-1 bg-white border rounded shadow-md">
               <Link to="/demo-1" className="block px-4 py-2 hover:bg-gray-100">
                 Demo 1
@@ -48,9 +57,23 @@ const Header = ({ active }) => {
                 Demo 3
               </Link>
             </div>
+            </>
+          
           )}
         </div>
+        <div className="md:hidden">
+          <button className="button flex items-center border p-2 px-4 rounded-md w-auto" onClick={toggleMenu}>
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 cursor-pointer" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16m-7 6h7" />
+            </svg>
+          </button>
+        </div>
       </div>
+      {menuOpen && (
+        
+          <Navbar active={3} />
+     
+      )}
     </div>
   );
 };
--- frontend/src/components/Layout/Navbar.jsx
+++ frontend/src/components/Layout/Navbar.jsx
@@ -5,22 +5,25 @@ import styles from '../../styles/styles';
 
 const Navbar = ({ active }) => {
   return (
-    <div className={`flex items-center justify-center 800px:${styles.normalFlex}`}>
-      {navItems &&
-        navItems.map((item, index) => (
-          <div key={index} className="mr-4  flex items-center">
-            <img src={item.logo} alt={item.title} className="h-6 w-6 mr-2" />
-            <Link
-              to={item.url}
-              className={`px-4 py-2 font-[500] cursor-pointer rounded-full 
-                ${active === index + 1 ? 'bg-gray-200 text-black shadow-lg' : 'text-black 800px:text-[#676666]'} 
-                800px:shadow-none active:text-[#17dd1f] active:shadow-lg transition-all duration-300 ease-in-out`}
-            >
-              {item.title}
-            </Link>
-          </div>
-        ))}
-    </div>
+    <>
+      {/* Nav items */}
+      <div className={`flex flex-col items-end md:items-center md:justify-center md:flex md:flex-row ${styles.normalFlex}`}>
+        {navItems &&
+          navItems.map((item, index) => (
+            <div key={index} className=" flex items-center">
+              <img src={item.logo} alt={item.title} className="h-6 w-6 mr-2" />
+              <Link
+                to={item.url}
+                className={`px-4 py-2 font-[500] cursor-pointer rounded-full 
+                  ${active === index + 1 ? 'bg-gray-200 text-black shadow-lg' : 'text-black 800px:text-[#676666]'} 
+                  800px:shadow-none active:text-[#17dd1f] active:shadow-lg transition-all duration-300 ease-in-out`}
+              >
+                {item.title}
+              </Link>
+            </div>
+          ))}
+      </div>
+    </>
   );
 };
 
