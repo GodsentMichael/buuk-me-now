597df20329dca6a2381bac755c67d6554d6bfed4
Godsent Michael <gentlemotivator@gmail.com>
2024-03-01 12:39:12 +0100

    removed the trailing slash in the origin property of the cors object

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,7 @@ app.use(cookieParser());
 // CORS
 app.use(cors({
     // origin: 'http://localhost:5173',
-    origin: 'https://buuk-me-now-delta.vercel.app/',
+    origin: 'https://buuk-me-now-delta.vercel.app',
     credentials: true
   }));
 
